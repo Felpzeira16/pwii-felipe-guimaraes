99541df6bd3cc9904d439b3453d96445a48c733c
Felipe Gusmão <felipegusmguim@gmail.com>
2025-02-25 16:31:49 -0300

    Aula de PHP com exemplos de concatenação de variaveis

---
+++ aula 01/ola.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+            echo "Hello World!<br>";
+           //oi tudo bem? DANCE ARIBITUBATITIUBU
+           $variavel = "Felipe";
+           $num = 3;
+           $letra = "Gusmão";
+
+           echo $variavel.'<br>';
+           echo $num.'<br>';
+           ECHO $num . " ". $letra.'<br>';
+          echo "${variavel} <h1> ${num} </h1>";
+          echo $variavel ."<p>". $num ." ". $letra ."</p>"
+        ?>
+
+    </body>
+</html>
