c38974e9371803067a362e374ba9fb72e12f774f
felipe gusmao <felipegusmguim@gmail.com>
2025-05-06 16:29:23 -0300

    Novos exercicíos das aulas do Prof. Jõao Silles

---
+++ aula05/index.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+
+    <form action="mensagem.php" method="POST">
+        <label for="msg">Digite sua mensagem: </label>
+        <input type="text" name="msg" id="msg"><br>
+        <label for="nome">Digite sua nome: </label>
+        <input type="text" name="nome" id="nome"><br>
+        <input type="submit">
+    </form>
+
+</body>
+</html>
